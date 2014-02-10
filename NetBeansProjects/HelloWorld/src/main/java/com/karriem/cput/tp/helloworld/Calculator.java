4226e82370d34c02f2db15291126032566868a5c
Karriem Petersen <>
2014-02-10 21:33:37 +0200

    Calculator Example

---
+++ NetBeansProjects/HelloWorld/src/main/java/com/karriem/cput/tp/helloworld/Calculator.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.karriem.cput.tp.helloworld;
+
+/**
+ *
+ * @author uchiha-clan
+ */
+public class Calculator {
+    
+    public int add(int a, int b){
+        
+        return a+b;
+    }
+
+    public int multiply(int a, int b) {
+        
+        return a*b;
+    }
+
+    public int divide(int a, int b) {
+        
+        return a/b;
+    }
+
+    public int subtract(int a, int b) {
+        
+        return a-b;
+    }
+    
+    public String name(String name){
+                
+        return name;
+    }
+}
